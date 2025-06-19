06a2cbcf838fbd6119022c10598fb5981d2fce90
eunyeong-Cha <eunoiay0@naver.com>
2025-06-19 13:14:10 +0900

    Initial commit

---
+++ wholeWordGame/pictureWordGame/problems.js
@@ -0,0 +1,195 @@
+
+const problems = [
+  {
+    image: "images/그림_가지.png",
+    words: ["가지"]
+  },
+  {
+    image: "images/그림_개구리.png",
+    words: ["개구리"]
+  },
+  {
+    image: "images/그림_개미.png",
+    words: ["개미"]
+  },
+  {
+    image: "images/그림_거미.png",
+    words: ["거미"]
+  },
+  {
+    image: "images/그림_고구마.png",
+    words: ["고구마"]
+  },
+  {
+    image: "images/그림_고래.png",
+    words: ["고래"]
+  },
+  {
+    image: "images/그림_구두.png",
+    words: ["구두"]
+  },
+  {
+    image: "images/그림_기차.png",
+    words: ["기차"]
+  },
+  {
+    image: "images/그림_기타.png",
+    words: ["기타"]
+  },
+  {
+    image: "images/그림_나무.png",
+    words: ["나무"]
+  },
+  {
+    image: "images/그림_나비.pngg",
+    words: ["나비"]
+  },
+  {
+    image: "images/그림_다리.png",
+    words: ["다리"]
+  },
+  {
+    image: "images/그림_도토리.png",
+    words: ["도토리"]
+  },
+  {
+    image: "images/그림_두부.png",
+    words: ["두부"]
+  },
+  {
+    image: "images/그림_마스크.png",
+    words: ["마스크"]
+  },
+  {
+    image: "images/그림_매미.png",
+    words: ["매미"]
+  },
+  {
+    image: "images/그림_모기.png",
+    words: ["모기"]
+  },
+  {
+    image: "images/그림_모자.png",
+    words: ["모자"]
+  },
+  {
+    image: "images/그림_무지개.png",
+    words: ["무지개"]
+  },
+  {
+    image: "images/그림_바구니.png",
+    words: ["바구니"]
+  },
+  {
+    image: "images/그림_바나나.png",
+    words: ["바나나"]
+  },
+  {
+    image: "images/그림_바다.png",
+    words: ["바다"]
+  },
+  {
+    image: "images/그림_바지.png",
+    words: ["바지"]
+  },
+  {
+    image: "images/그림_버스.png",
+    words: ["버스"]
+  },
+  {
+    image: "images/그림_보라.png",
+    words: ["보라"]
+  },
+  {
+    image: "images/그림_사이다.png",
+    words: ["사이다"]
+  },
+  {
+    image: "images/그림_사자.png",
+    words: ["사자"]
+  },
+  {
+    image: "images/그림_새우.png",
+    words: ["새우"]
+  },
+  {
+    image: "images/그림_시소.png",
+    words: ["시소"]
+  },
+  {
+    image: "images/그림_아파트.png",
+    words: ["아파트"]
+  },
+  {
+    image: "images/그림_야구.png",
+    words: ["야구"]
+  },
+  {
+    image: "images/그림_여우.png",
+    words: ["여우"]
+  },
+  {
+    image: "images/그림_오리.png",
+    words: ["오리"]
+  },
+  {
+    image: "images/그림_오이.png",
+    words: ["오이"]
+  },
+  {
+    image: "images/그림_우유.png",
+    words: ["우유"]
+  },
+  {
+    image: "images/그림_자두.png",
+    words: ["자두"]
+  },
+  {
+    image: "images/그림_지우개.png",
+    words: ["지우개"]
+  },
+  {
+    image: "images/그림_커피.png",
+    words: ["커피"]
+  },
+  {
+    image: "images/그림_쿠키.png",
+    words: ["쿠키"]
+  },
+  {
+    image: "images/그림_타조.png",
+    words: ["타조"]
+  },
+  {
+    image: "images/그림_토마토.png",
+    words: ["토마토"]
+  },
+  {
+    image: "images/그림_파리.png",
+    words: ["파리"]
+  },
+  {
+    image: "images/그림_포도.png",
+    words: ["포도"]
+  },
+  {
+    image: "images/그림_피아노.png",
+    words: ["피아노"]
+  },
+  {
+    image: "images/그림_피자.png",
+    words: ["피자"]
+  },
+  {
+    image: "images/그림_하나.png",
+    words: ["하나"]
+  },
+  {
+    image: "images/그림_하마.png",
+    words: ["하마"]
+  },
+  {
+    image: "images/그림_호두.png",
+    words: ["호두"]
+  }
+];
---
+++ wholeWordGame/splitWordGame/problems.js
@@ -0,0 +1,350 @@
+const problems = [
+  {
+    "image": "images/그림_가지.png",
+    "letters": [
+      "가",
+      "지"
+    ]
+  },
+  {
+    "image": "images/그림_개구리.png",
+    "letters": [
+      "개",
+      "구",
+      "리"
+    ]
+  },
+  {
+    "image": "images/그림_개미.png",
+    "letters": [
+      "개",
+      "미"
+    ]
+  },
+  {
+    "image": "images/그림_거미.png",
+    "letters": [
+      "거",
+      "미"
+    ]
+  },
+  {
+    "image": "images/그림_고구마.png",
+    "letters": [
+      "고",
+      "구",
+      "마"
+    ]
+  },
+  {
+    "image": "images/그림_고래.png",
+    "letters": [
+      "고",
+      "래"
+    ]
+  },
+  {
+    "image": "images/그림_구두.png",
+    "letters": [
+      "구",
+      "두"
+    ]
+  },
+  {
+    "image": "images/그림_기차.png",
+    "letters": [
+      "기",
+      "차"
+    ]
+  },
+  {
+    "image": "images/그림_기타.png",
+    "letters": [
+      "기",
+      "타"
+    ]
+  },
+  {
+    "image": "images/그림_나무.png",
+    "letters": [
+      "나",
+      "무"
+    ]
+  },
+  {
+    "image": "images/그림_나비.png",
+    "letters": [
+      "나",
+      "비"
+    ]
+  },
+  {
+    "image": "images/그림_다리.png",
+    "letters": [
+      "다",
+      "리"
+    ]
+  },
+  {
+    "image": "images/그림_도토리.png",
+    "letters": [
+      "도",
+      "토",
+      "리"
+    ]
+  },
+  {
+    "image": "images/그림_두부.png",
+    "letters": [
+      "두",
+      "부"
+    ]
+  },
+  {
+    "image": "images/그림_마스크.png",
+    "letters": [
+      "마",
+      "스",
+      "크"
+    ]
+  },
+  {
+    "image": "images/그림_매미.png",
+    "letters": [
+      "매",
+      "미"
+    ]
+  },
+  {
+    "image": "images/그림_모기.png",
+    "letters": [
+      "모",
+      "기"
+    ]
+  },
+  {
+    "image": "images/그림_모자.png",
+    "letters": [
+      "모",
+      "자"
+    ]
+  },
+  {
+    "image": "images/그림_무지개.png",
+    "letters": [
+      "무",
+      "지",
+      "개"
+    ]
+  },
+  {
+    "image": "images/그림_바구니.png",
+    "letters": [
+      "바",
+      "구",
+      "니"
+    ]
+  },
+  {
+    "image": "images/그림_바나나.png",
+    "letters": [
+      "바",
+      "나",
+      "나"
+    ]
+  },
+  {
+    "image": "images/그림_바다.png",
+    "letters": [
+      "바",
+      "다"
+    ]
+  },
+  {
+    "image": "images/그림_바지.png",
+    "letters": [
+      "바",
+      "지"
+    ]
+  },
+  {
+    "image": "images/그림_버스.png",
+    "letters": [
+      "버",
+      "스"
+    ]
+  },
+  {
+    "image": "images/그림_보라.png",
+    "letters": [
+      "보",
+      "라"
+    ]
+  },
+  {
+    "image": "images/그림_사이다.png",
+    "letters": [
+      "사",
+      "이",
+      "다"
+    ]
+  },
+  {
+    "image": "images/그림_사자.png",
+    "letters": [
+      "사",
+      "자"
+    ]
+  },
+  {
+    "image": "images/그림_새우.png",
+    "letters": [
+      "새",
+      "우"
+    ]
+  },
+  {
+    "image": "images/그림_시소.png",
+    "letters": [
+      "시",
+      "소"
+    ]
+  },
+  {
+    "image": "images/그림_아파트.png",
+    "letters": [
+      "아",
+      "파",
+      "트"
+    ]
+  },
+  {
+    "image": "images/그림_야구.png",
+    "letters": [
+      "야",
+      "구"
+    ]
+  },
+  {
+    "image": "images/그림_여우.png",
+    "letters": [
+      "여",
+      "우"
+    ]
+  },
+  {
+    "image": "images/그림_오리.png",
+    "letters": [
+      "오",
+      "리"
+    ]
+  },
+  {
+    "image": "images/그림_오이.png",
+    "letters": [
+      "오",
+      "이"
+    ]
+  },
+  {
+    "image": "images/그림_우유.png",
+    "letters": [
+      "우",
+      "유"
+    ]
+  },
+  {
+    "image": "images/그림_자두.png",
+    "letters": [
+      "자",
+      "두"
+    ]
+  },
+  {
+    "image": "images/그림_지우개.png",
+    "letters": [
+      "지",
+      "우",
+      "개"
+    ]
+  },
+  {
+    "image": "images/그림_커피.png",
+    "letters": [
+      "커",
+      "피"
+    ]
+  },
+  {
+    "image": "images/그림_쿠키.png",
+    "letters": [
+      "쿠",
+      "키"
+    ]
+  },
+  {
+    "image": "images/그림_타조.png",
+    "letters": [
+      "타",
+      "조"
+    ]
+  },
+  {
+    "image": "images/그림_토마토.png",
+    "letters": [
+      "토",
+      "마",
+      "토"
+    ]
+  },
+  {
+    "image": "images/그림_파리.png",
+    "letters": [
+      "파",
+      "리"
+    ]
+  },
+  {
+    "image": "images/그림_포도.png",
+    "letters": [
+      "포",
+      "도"
+    ]
+  },
+  {
+    "image": "images/그림_피아노.png",
+    "letters": [
+      "피",
+      "아",
+      "노"
+    ]
+  },
+  {
+    "image": "images/그림_피자.png",
+    "letters": [
+      "피",
+      "자"
+    ]
+  },
+  {
+    "image": "images/그림_하나.png",
+    "letters": [
+      "하",
+      "나"
+    ]
+  },
+  {
+    "image": "images/그림_하마.png",
+    "letters": [
+      "하",
+      "마"
+    ]
+  },
+  {
+    "image": "images/그림_호두.png",
+    "letters": [
+      "호",
+      "두"
+    ]
+  }
+];
